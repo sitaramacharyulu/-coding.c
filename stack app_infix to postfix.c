1093d178322ab7fed345cac920b12ed3944b0b26
vishnupriya <147472761+sitaramacharyulu@users.noreply.github.com>
2024-04-30 09:37:19 +0530

    Add files via upload

---
+++ stack app_infix to postfix.c
@@ -0,0 +1,47 @@
+//2+3*4-->int its infix form
+//infix:operators are in placed in between operands
+//(2+3)*4-->prefix:
+//prefix:operators will be place infront of operands
+//A+B-->Infix form
+//+AB-->prefix form
+//AB+-->postfix form
+//COVERT INFIX INTO POSTFIX 
+#include <stdio.h>
+#include<string.h>
+int get_priority(char ch){
+    if(ch=='/' || ch=='*') return 3;
+    else if(ch=='+' || ch=='-')return 2;
+    else return 1;
+}
+void convert_infix_to_postfix(char *infix){
+    char postfix[101];
+    char st[101];
+    int top=-1,i,j=0;
+    for(i=0;i<strlen(infix);i++){
+        int priority=get_priority(infix[i]);
+        if(priority==1){//operend
+            postfix[j++]=infix[i];//post-increment
+        }
+        else{
+            while(top!=-1 && priority<=get_priority(st[top])){
+                postfix[j++]=st[top];
+                top--;
+            }
+            top++;
+            st[top]=infix[i];
+        }
+    }
+    while(top!=-1) {
+    postfix[j]=st[top];
+    j++;
+    top--;
+    }
+    postfix[j]='\0';
+    
+        printf("%s",postfix);
+}
+int main() {
+char infix[101];
+scanf("%s",infix);
+convert_infix_to_postfix(infix);
+}
